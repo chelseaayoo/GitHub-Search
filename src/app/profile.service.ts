4522bc5980c0b2f533d3f9c3ba4cf6b4df05d4ed
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-17 03:48:14 -0400

    import HttpClient, get profile information

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+// import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username!: string;
+  private clientid = 'b432ed6627b1c50bcf93';
+  private clientsecret = 'c72c7a4a83e003bc86dd42d6823b75f7ba04bc06'
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    alert ('service is ready');
+    this.username = 'chelseaayoo';
+  } 
+  getProfileInformation(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" +this.clientid + "&client_secret=" +this.clientsecret )
+    .pipe(map(response =>console.log(response)))
+  }  
 }
